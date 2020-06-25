ed3ed097a6f9fff9074e82110d0dbc84d5b0c9a9
Makoto Aoyama <m@aoyama.dev>
2020-06-25 16:46:36 +0900

    Initial Commit

---
+++ SystemLayoutSizeFittingSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SystemLayoutSizeFittingSample
+//
+//  Created by Makoto Aoyama on 2020/06/25.
+//  Copyright © 2020 dev.aoyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SystemLayoutSizeFittingSampleTests/SystemLayoutSizeFittingSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  SystemLayoutSizeFittingSampleTests.swift
+//  SystemLayoutSizeFittingSampleTests
+//
+//  Created by Makoto Aoyama on 2020/06/25.
+//  Copyright © 2020 dev.aoyama. All rights reserved.
+//
+
+import XCTest
+@testable import SystemLayoutSizeFittingSample
+
+class SystemLayoutSizeFittingSampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
